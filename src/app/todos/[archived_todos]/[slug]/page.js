b25b3b76bf2bdcb1b90ba3ff6b346e9b9a14c7b1
Muhammad Nauman <MUHAMMADNAUMANBILAL@gmail.com>
2024-09-10 12:46:18 +0500

    Update page.js

--- src/app/todos/[archived_todos]/[slug]/page.js
+++ src/app/todos/[archived_todos]/[slug]/page.js
@@ -1,6 +1,12 @@
-export default function ArchivedTodos () {
 
-    return(<>
-        Lists all Archived Todos
+
+
+"use client";  // This makes the component a Client Component
+import { useParams } from "next/navigation"
+
+export default function ArchivedTodos () {
+    const {slug, archived_todos} = useParams()
+    return (<>
+        child todo {archived_todos} and id = {slug}
     </>)
 }
